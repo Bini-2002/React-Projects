2707ef59ff3db6f5eda874e6042457964ad885ec
Biniyam Getachew <binigetachew18@gmail.com>
2025-06-24 11:16:13 +0300

    Add main components: Header, Footer, Facts, and MainContent; update styles

---
+++ React-facts/react-facts/src/Footer.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+
+export function Footer() {
+  return (
+    <footer>
+      <p>© 2025 React Facts. All rights reserved.</p>
+      <p>Created by Biniyam Getachew</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ React-facts/react-facts/src/MainContent.jsx
@@ -0,0 +1,18 @@
+
+import './index.css'
+
+
+export function ManiContent() {
+  return(
+  <main>
+    <h1>Exiting thigs about to learn react</h1>
+    <ol>
+        <li>It's is easy</li>
+        <li>It has large community</li>
+        <li>It's is job demanding</li>
+    </ol>
+  </main>
+  )
+}
+
+export default ManiContent;
