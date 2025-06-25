1c2995fa73430e72dbe11d157741e74bf1753a69
Biniyam Getachew <binigetachew18@gmail.com>
2025-06-25 11:24:00 +0300

    Innitial Commit for new project "Travel Journal".

---
+++ Travel-Journal/src/components/Entry.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="https://scrimba.com/links/travel-journal-logo-url" alt="Travel Journal Logo" className="header-logo" />
+            <h1 className="header-title">my travel journal.</h1>
+        </header>
+    )
+}
