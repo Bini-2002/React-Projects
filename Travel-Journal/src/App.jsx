f0f14f312f92cecb06846265d6dbd4ca24725411
Biniyam Getachew <binigetachew18@gmail.com>
2025-06-25 23:11:07 +0300

    add travel journal entry for Mount Fuji with dynamic props and remove outdated markdown file

--- Travel-Journal/src/App.jsx
+++ Travel-Journal/src/App.jsx
@@ -6,7 +6,14 @@ export default function App() {
   return (
     <>
       <Header/>
-      <Entry/>
+      <Entry 
+      img = {"https://scrimba.com/links/travel-journal-japan-image-url"}
+      title = {"Mount Fuji"}
+      country = {"Japan"}
+      googleMapsLink = {"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"}
+      dates = {"12 Jan, 2021 - 24 Jan, 2021"}
+      text = {"Mount Fuji is Japan's highest peak, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707. The mountain is a UNESCO World Heritage Site and is considered one of Japan's three sacred mountains. It is a popular destination for both tourists and climbers, offering stunning views and a unique cultural experience."}
+      />
     </>
   )
 
--- Travel-Journal/src/components/Entry.jsx
+++ Travel-Journal/src/components/Entry.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
 import headerlogo from '../images/marker.png'; // Adjust the path as necessary
 import '../index.css'; // Assuming you have a CSS file for styling
 
-
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="entry">
       <div className="entry-image-container">
-        <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" className="entry-image" />
+        <img src={props.img} alt="Mount Fuji" className="entry-image" />
       </div>
       <div className="entry-details">
         <div className="entry-location">
           <img src={headerlogo} alt="Location Icon" className="location-icon" />
-          <span className="entry-country">Japan</span>
-          <a href="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9" target="_blank" rel="noopener noreferrer" className="entry-map-link">View on Google Maps</a>
+          <span className="entry-country">{props.country}</span>
+          <a href={props.googleMapsLink} target="_blank" rel="noopener noreferrer" className="entry-map-link">View on Google Maps</a>
         </div>
-        <h2 className="entry-title">Mount Fuji</h2>
-        <p className="entry-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="entry-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
 
     </article>
