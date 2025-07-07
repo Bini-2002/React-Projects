59cbb6e73b3acd36a6bb81663716e2108338ec9c
Biniyam Getachew <binigetachew18@gmail.com>
2025-07-07 17:54:24 +0300

    refactor: move ingredient list rendering to IngredientList component

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -8,9 +8,7 @@ export default function Main() {
     const [ingredients, setIngredients] = React.useState(["all the main spices", "pasta", "ground beef", "tomato pasta"])
     const [recipeShown, setRecipeShown] = React.useState(false)
     
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
@@ -38,18 +36,10 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {ingredients.length > 0 && <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length >= 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={toggleRecipeShown}>Get a recipe</button>
-                </div>
-                }
-            </section>
+            {ingredients.length > 0 && 
+            <IngredientList 
+                ingredients={ingredients} 
+                toggleRecipeShown = {toggleRecipeShown} />
             }
             {
             recipeShown && <ClaudeRecipe/>
