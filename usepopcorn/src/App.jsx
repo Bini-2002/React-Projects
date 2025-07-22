e15e9a0f732d315ff4f75fad5c52591ad66a8616
Biniyam Getachew <binigetachew18@gmail.com>
2025-07-22 15:53:30 +0300

    pass movies data to NavBar, Main, NumResult, ListBox, and MovieList components

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -52,23 +52,24 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
 
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 }
 
-function NavBar() {
+function NavBar({movies}) {
 
 
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResult />
+      <NumResult movies={movies} />
     </nav>
   );
 }
@@ -97,27 +98,27 @@ function Search() {
   )
 }
 
-function NumResult() {
+function NumResult({movies}) {
   return (
     <p className="num-results">
-      Found <strong>x</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   )
 }
 
 
-function Main() {
+function Main( {movies}) {
 
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
   )
 
 }
 
-function ListBox() {
+function ListBox({movies}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -130,7 +131,7 @@ function ListBox() {
         {isOpen1 ? "–" : "+"}
       </button>
       {isOpen1 && (
-        <MovieList />
+        <MovieList movies={movies} />
       )}
     </div>
 
@@ -138,8 +139,7 @@ function ListBox() {
 }
 
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({movies}) {
 
   return (
     <ul className="list">
