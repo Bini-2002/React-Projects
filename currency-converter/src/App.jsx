4720e7f2c16d67d4f73759246e488a269cee7406
Biniyam Getachew <binigetachew18@gmail.com>
2025-08-07 19:25:37 +0300

    implement currency conversion functionality and update input handling in App component

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { use } from 'react';
 import './App.css';
+import { useState , useEffect } from 'react';
 
 export default function App() {
+
+  const [ammount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('EUR');
+  const [exchangeRate, setExchangeRate] = useState(0);
+
+
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(Number(e.target.value));
+  };
+
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(Number(e.target.value));
+  };
+
+  useEffect(() => {
+    async function convert() {
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${ammount}&from=${fromCurrency}&to=${toCurrency}`);
+      const data = await res.json();
+      setExchangeRate(data.rates[toCurrency]);
+    }
+    convert();
+  }, [fromCurrency, toCurrency]);
+
+
   return(
     <div>
-      <input type="text" />
-      <select>
+      <input type="text" value={ammount} onChange={handleAmountChange} />
+      <select value={fromCurrency} onChange={handleFromCurrencyChange}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={toCurrency} onChange={handleToCurrencyChange}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>OUTPUT: {convertedAmount} {toCurrency}</p>
     </div>
   )
 }
