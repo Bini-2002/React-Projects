261b0071862770764c37fc18af8de02f98e67840
Biniyam Getachew <binigetachew18@gmail.com>
2025-08-17 11:04:33 +0300

    feat: integrate json-server for quiz questions and refactor App component
    - Added json-server as a dependency to serve quiz questions from a local JSON file.
    - Refactored App component to use useReducer for managing questions state and loading status.
    - Removed hardcoded questions.json file from src and moved it to data directory for json-server.
    - Updated package.json scripts to include a command for running the json-server.

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,15 +1,48 @@
 import Header from './Header';
 import Main from './Main';
+import { useReducer, useEffect } from 'react';
+
+const initialState = {
+  questions: [],
+  // 'loading' , 'error', 'ready' , 'active' , 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setQuestions":
+      return { 
+        ...state, 
+        questions: action.payload, 
+        status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
 
 export default function App() {
+  const [state , dispatch] = useReducer(reducer , initialState)
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "setQuestions", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
-      <Header/>
+      <Header />
 
-    <Main>
-      <p>1/15</p>
-      <p>Questions</p>
-    </Main>
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
