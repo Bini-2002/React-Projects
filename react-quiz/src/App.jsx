13d0026c583bfd59c977b8cd40f81bb686c36952
Biniyam Getachew <binigetachew18@gmail.com>
2025-08-17 10:08:06 +0300

    Update Header component size and create Main component skeleton

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <div className="app">
       <Header/>
+
+      
     </div>
   );
 }
--- react-quiz/src/Header.jsx
+++ react-quiz/src/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Header() {
   return (
     <header className='app-header'>
-      <FaReact size="5rem" color="#61DAFB" />
+      <FaReact size="10rem" color="#61DAFB" />
       <h1>The React Quiz</h1>
     </header>
   );
